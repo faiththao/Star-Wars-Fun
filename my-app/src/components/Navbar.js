45a9e305e3e56d84000330d31041ce2b8b3d503b
Faith <thao.faith2@gmail.com>
2021-08-23 10:06:46 -0600

    added router

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,5 +1,17 @@
 import { NavLink } from "react-router-dom"
 
 export default function Navbar() {
-
+    return (
+        <div className="navbar">
+            <NavLink to="/">
+                Home
+            </NavLink>
+            <NavLink to="/players">
+                Players
+            </NavLink>
+            <NavLink to="/create-player">
+                Create a Player
+            </NavLink>
+        </div>
+    )
 }
