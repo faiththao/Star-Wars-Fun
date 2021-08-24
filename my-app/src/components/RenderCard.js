aef119cf68ca3699a449cc721be8ce409509540e
alec <kaltschmidalec@gmail.com>
2021-08-24 14:36:07 -0700

    rendered characters and slight change to navbar

--- my-app/src/components/RenderCard.js
+++ my-app/src/components/RenderCard.js
@@ -3,7 +3,7 @@ export default function ({ character }) {
     const {uid, url, name} = character
 
     return (
-        <div className="PlayerCard">
+        <div className="RenderCard">
             <img src={url} alt={name} className="img" />
             <h2>Name: {name}</h2>
         </div>
