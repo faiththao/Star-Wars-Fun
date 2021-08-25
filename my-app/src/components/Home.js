3218d6295f2ebca8724d06e009623f229a4190d5
alec <kaltschmidalec@gmail.com>
2021-08-25 14:47:34 -0700

    correct timestamp

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -4,10 +4,10 @@ const May42022Timestamp = 1659983662000
 
 export default function Home() {
     return (
-        <div>
+        <div className="homeContainer">
             <h1>Home Page</h1>
             <img src="https://learn.corel.com/wp-content/uploads/2018/04/may-the-fourth-blog-feature-image.jpg" alt="May the Fourth"/>
-            <CountDown countdownTimestampMs={1659983662000}  />
+            <CountDown countdownTimestampMs={1651647600000}  />
         </div>
     )
 }
